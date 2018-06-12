d36cbb7f5f50802792684b29160e97dc6ffd746d
johnale <johnale@cisco.com>
2018-06-12 10:10:34 -0400

    first node.js update add webServer

---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const handlebars = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+handlebars.registerPartials(__dirname + '/views/partials');
+app.set('view engine', handlebars);
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs', {
+//     pageTitle: 'Maintenance Page'
+//   });
+// });
+
+app.use(express.static(__dirname + '/public')); //use middleware
+
+handlebars.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+handlebars.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (request, response) => {
+  // response.send('<h1>Hello Express!</h1>');
+  // response.send({
+  //   name: 'John',
+  //   likes: [
+  //     'Golf',
+  //     'basketball',
+  //     'family'
+  //   ]
+  response.render('home.hbs', {
+    pageTitle: 'Home Page',
+    // currentYear: new Date().getFullYear(),
+    homePageMessage: 'Hello, welcome to my Home Page!'
+  });
+});
+
+app.get('/about', (request, response) => {
+  response.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/bad', (request, response) => {
+  response.send({
+    errorMessage: 'Unable to fulfill this request.'
+  });
+});
+
+// app.get('/maintenance', (req, res) => {
+//   res.render('maintenance.hbs', {
+//     pageTitle: 'Maintenance Page'
+//   });
+// });
+
+app.listen(3000, () => {
+  console.log('Server is up and running on port 3000.');
+});
